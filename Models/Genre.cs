967f4b1a23dbd4d016b970c6af12ece59f604e4b
mwollek <mwollek@student.agh.edu.pl>
2021-03-16 22:05:01 +0100

    Modified Models for EF

--- Models/Genre.cs
+++ Models/Genre.cs
@@ -7,7 +7,10 @@ namespace MvcMusicStore.Models
 {
     public class Genre
     {
+        public int GenreId { get; set; }
         public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Album> Albums { get; set; }
 
     }
 }   
