9a2b5ccfdad83b65f40781bcf9744f3ee42e85ea
mwollek <mwollek@student.agh.edu.pl>
2021-03-18 11:35:15 +0100

    Added Administrator user

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using MvcMusicStore.Models;
 using Owin;
+using System;
 
 [assembly: OwinStartupAttribute(typeof(MvcMusicStore.Startup))]
 namespace MvcMusicStore
@@ -9,6 +13,43 @@ namespace MvcMusicStore
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesandUsers();
+
+
+        }
+
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+
+                // first we create Admin rool    
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+
+                //Here we create a Admin super user who will maintain the website                   
+
+                var user = new ApplicationUser();
+                user.UserName = "admin@gmail.com";
+                user.Email = "admin@gmail.com";
+
+                string userPWD = "Admin!0";
+
+                var chkUser = UserManager.Create(user, userPWD);
+
+                //Add default User to Role Admin    
+                if (chkUser.Succeeded)
+                {
+                    var result1 = UserManager.AddToRole(user.Id, "Admin");
+                }
+            }
         }
     }
 }
