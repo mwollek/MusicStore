9d4e40d33b8e946d6db30a405c355318059b7e26
mwollek <mwollek@student.agh.edu.pl>
2021-03-18 20:54:07 +0100

    Added Checkout controller

---
+++ Controllers/CheckoutController.cs
@@ -0,0 +1,67 @@
+﻿using MvcMusicStore.Models;
+using MvcMusicStore.Models.Own;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    [Authorize]
+    public class CheckoutController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        const string PromoCode = "FREE";
+
+
+        // GET: Checkout
+        public ActionResult AddressAndPayment(FormCollection values)
+        {
+            var order = new Order();
+            TryUpdateModel(order);
+            try
+            {
+                if (string.Equals(values["PromoCode"], PromoCode, StringComparison.OrdinalIgnoreCase) == false)
+                {
+                    return View(order);
+                }
+                else
+                {
+                    order.Username = User.Identity.Name;
+                    order.OrderDate = DateTime.Now;
+
+                    db.Orders.Add(order);
+                    db.SaveChanges();
+
+                    var cart = ShoppingCart.GetCart(this.HttpContext);
+                    cart.CreateOrder(order);
+
+                    return RedirectToAction("Complete", new { id = order.OrderId });
+                }
+            }
+            catch 
+            {
+
+                return View();
+            }
+            
+        }
+
+        public ActionResult Complete(int id)
+        {
+            // Validate customer owns this order
+            bool isValid = db.Orders.Any(
+            o => o.OrderId == id &&
+            o.Username == User.Identity.Name);
+            if (isValid)
+            {
+                return View(id);
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+    }
+}
